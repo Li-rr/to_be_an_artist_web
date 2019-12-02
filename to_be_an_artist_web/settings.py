bf0a3a2d58d949d0dd91af2b95b0dfbe82d17b50
*** <921090271@qq.com>
2019-12-03 02:21:41 +0800

    解决跨域问题

--- to_be_an_artist_web/settings.py
+++ to_be_an_artist_web/settings.py
@@ -43,13 +43,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+# 解决跨域问题
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'to_be_an_artist_web.urls'
 
 TEMPLATES = [

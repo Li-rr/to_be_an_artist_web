83ac16c893cacee2f4e812cb198d8211280d64b8
*** <921090271@qq.com>
2019-12-03 02:59:15 +0800

    修改前端路由

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Header from '@/compoents/First'
+import First from "../components/First";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'First',
+      component: First
     }
   ]
 })

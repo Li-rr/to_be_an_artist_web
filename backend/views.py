2c8805126eb7b0e91837174ce8e8e3df6ab25218
*** <921090271@qq.com>
2019-12-03 02:23:32 +0800

    前期准备工作

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import json,sys
+import json,sys,os
 import pickle
 # Create your views here.
+
+from backend.fuck_poe.network_model import poetry_network
+from backend.fuck_poe.fuck_generate import gen_poetry
+
+cur_file = os.path.abspath(sys.argv[0])
+cur_path = os.path.dirname(cur_file)
+

d13e2fa294e875ac5fa8f7f65ed06ba8df219495
*** <921090271@qq.com>
2019-12-03 13:37:55 +0800

    修改数据库，创建数据模型

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=30)
+    passward = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.username

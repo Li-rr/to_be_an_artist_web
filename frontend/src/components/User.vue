1c18ca2ecf7e1ad8f910135bcf13f701f369d15d
*** <921090271@qq.com>
2019-12-04 22:38:06 +0800

    动态显示组件

--- frontend/src/components/User.vue
+++ frontend/src/components/User.vue
@@ -1,6 +1,7 @@
 <template>
-    <div id="user">
-      {{msg}}
+    <div id="user" v-show="childUser">
+      <h1>用户名：{{msg}}</h1>
+      <h1>{{childUser}}</h1>
     </div>
 </template>
 
@@ -9,12 +10,21 @@
         name: "User",
         data(){
           return{
-              msg: 'fuck you too'
+              msg: 'fuck you too',
+              // showDeal : this.childUser
           }
         },
         mounted() {
-            alert("-> "+this.$route.query.username)
-            this.msg = this.$route.query.username
+            //this.showDeal = true
+            //alert("-> "+this.$route.query.username)
+            //this.msg = this.$route.query.username
+        },
+        props:['childUser'],
+        watch: {
+            // 监听属性
+            childUser:function (newVal,oldVal) {
+                alert('新值 '+newVal + ' 旧值 '+oldVal)
+            }
         }
 
     }
